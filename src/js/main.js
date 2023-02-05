8a179ba73597d97715ae8fd911a2e420c4aa8ca6
EterNorSos <norbert.sosnowski@outlook.com>
2023-02-05 23:31:42 +0100

    Changing button to tree of a>button

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import "../scss/main.scss";
-import FormAuth_HTMLJS from "./formHTMLJS";
-
-document.addEventListener("DOMContentLoaded", (e) => {
-  (() => {
-    const form1 = new FormAuth_HTMLJS(".form");
-  })();
-});
