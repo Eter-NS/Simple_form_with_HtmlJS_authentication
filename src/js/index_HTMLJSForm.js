105c342149bb01f78089db0a1111e2bb2a39f988
EterNorSos <norbert.sosnowski@outlook.com>
2023-02-05 23:32:11 +0100

    renamed index to index_HTMLJSForm

---
+++ src/js/index_HTMLJSForm.js
@@ -0,0 +1,8 @@
+import "../scss/main.scss";
+import FormAuth_HTMLJS from "./formHTMLJS";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  (() => {
+    const form1 = new FormAuth_HTMLJS(".form");
+  })();
+});
